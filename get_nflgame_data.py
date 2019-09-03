b508a5c47a83fca21fe1779f2358fb47c6841126
Jeff Bruce <jeffbruce@gmail.com>
2019-09-03 14:19:32 -0400

    remove file

--- get_nflgame_data.py
+++
@@ -1,7 +0,0 @@
-# This seems to work after I modified seq.py so that it works with Python3.  Maybe I should just use Python2.7 for this project.
-import nflgame
-
-games = nflgame.games(2018, week=None)
-plays = nflgame.combine_plays(games)  # GenPlays object
-player_data = plays.players()  # GenPlayerStats object
-player_data.csv('player_data.csv')
