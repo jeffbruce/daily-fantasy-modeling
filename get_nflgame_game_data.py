7672816b591ad87aeb9e4516079ef1d126232388
Jeff Bruce <jeffbruce@gmail.com>
2019-09-05 20:05:33 -0400

    modify script to get nflgame data

--- get_nflgame_game_data.py
+++ get_nflgame_game_data.py
@@ -2,13 +2,16 @@ import os
 os.chdir('nflgame')  # TODO: Relative import not working from top level directory, so this is a hack around it.
 import nflgame
 
-years = [2017, 2018]
+years = [2018]
 weeks = [i+1 for i in range(17)]
 
 for y in years:
 	for w in weeks:
 		# TODO: Figure out why y=2018 with w=4 doesn't work here.
-		games = nflgame.games(y, w)
-		plays = nflgame.combine_plays(games)
-		player_data = plays.players()
-		player_data.csv('../data/nflgame/game-level-raw/'+str(y)+'-'+str(w)+'-'+'game-level-data.csv')
+		if y == 2018 and w == 4:
+			pass
+		else:
+			games = nflgame.games(y, w)
+			plays = nflgame.combine_plays(games)
+			player_data = plays.players()
+			player_data.csv('../data/nflgame/game-level-raw/'+str(y)+'-'+str(w)+'-'+'game-level-data.csv', allfields=True)
