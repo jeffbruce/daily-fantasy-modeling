6995e1bf3f7dc40ff2361318b9d07c611a920986
Jeff Bruce <jeffbruce@gmail.com>
2019-09-25 11:53:25 -0400

    add data, rename some files

--- README.md
+++ README.md
@@ -6,5 +6,9 @@ Modeling daily fantasy NFL football to predict the amount of points players will
 - nflgame: An open API to pull raw NFL game, player, and play-level data.
 - top level directory: Where the analyses are found.
 
+## Updating the Data
+- draftkings: Either run Airflow DAG or paste HTML files in the raw directory, then run parse_historic_draftkings_data.py.
+- nflgame: Open up ipython in the top level directory.  Copy/paste the get_nflgame_game_data.py script to pull data files with the desired inputs.
+
 ## Analyses
 Core analysis can be found in [draftkings_modeling.ipynb](https://github.com/jeffbruce/daily-fantasy-modeling/blob/master/draftkings_modeling.ipynb).  If the link doesn't work, try [nbviewer](https://nbviewer.jupyter.org/), it seems to be more consistently capable of rendering jupyter notebooks.
--- get_nflgame_game_data.py
+++ get_nflgame_game_data.py
@@ -1,17 +1,20 @@
 import os
-os.chdir('nflgame')  # TODO: Relative import not working from top level directory, so this is a hack around it.
+# TODO: Relative import not working from top level directory, so this is a hack around it.
+os.chdir('nflgame')
+
 import nflgame
 
-years = [2018]
+years = [2019]
 weeks = [i+1 for i in range(17)]
 
 for y in years:
-	for w in weeks:
-		# TODO: Figure out why y=2018 with w=4 doesn't work here.
-		if y == 2018 and w == 4:
-			pass
-		else:
-			games = nflgame.games(y, w)
-			plays = nflgame.combine_plays(games)
-			player_data = plays.players()
-			player_data.csv('../data/nflgame/game-level-raw/'+str(y)+'-'+str(w)+'-'+'game-level-data.csv', allfields=True)
+    for w in weeks:
+        # TODO: Figure out why y=2018 with w=4 doesn't work here.
+        if y == 2018 and w == 4:
+            pass
+        else:
+            games = nflgame.games(y, w)
+            plays = nflgame.combine_plays(games)
+            player_data = plays.players()
+            player_data.csv('../data/nflgame/game-level-raw/'+str(y) +
+                            '-'+str(w)+'-'+'game-level-data.csv', allfields=True)
